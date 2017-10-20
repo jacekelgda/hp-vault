090b8a916552f4db3c0c87be9433c4bf2efa4b4f
jacekelgda <jacekelgda@gmail.com>
2017-10-20 17:13:26 +0200

    slack controller working

--- src/controllers/slashCommand.js
+++ src/controllers/slashCommand.js
@@ -3,7 +3,10 @@ import express from 'express'
 const router = new express.Router()
 
 router.post('/slash', async function (req, res) {
-    console.log(req)
+    console.log(req.body.text)
+    console.log(req.body.user_id)
+    console.log(req.body.user_name)
+    res.send('ok')
 })
 
 export default router
