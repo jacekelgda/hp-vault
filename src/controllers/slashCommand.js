149e967fbca1227fde6fd226312042698d58e57d
jacekelgda <jacekelgda@gmail.com>
2017-10-20 17:30:50 +0200

    wip

--- src/controllers/slashCommand.js
+++ src/controllers/slashCommand.js
@@ -2,10 +2,13 @@ import express from 'express'
 
 const router = new express.Router()
 
+const FITQUEST_COMMAND = '/fitquest-start'
+const FITQUEST_COMMAND_START_TEXT = 'start'
+
 router.post('/slash', async function (req, res) {
-    console.log(req.body.text)
-    console.log(req.body.user_id)
-    console.log(req.body.user_name)
+    if (req.body.command === FITQUEST_COMMAND && req.body.text === FITQUEST_COMMAND_START_TEXT) {
+      // fetch all users
+    }
     res.send('ok')
 })
 
